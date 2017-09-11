2ec9f8d5e8f659fd5c20dd12edadc17c0c2fce0a
raveit65 <mate@raveit.de>
2017-09-11 11:04:54 +0200

    fix loop initial declarations

--- src/mate-screensaver-preferences.c
+++ src/mate-screensaver-preferences.c
@@ -935,7 +935,7 @@ time_to_string_text (long time)
 	char *secs, *mins, *hours, *string;
 	int   sec, min, hour;
 
-	int   inc_len, len_minutes;
+	int n, inc_len, len_minutes;
 
 	sec = time % 60;
 	time = time - sec;
@@ -960,7 +960,7 @@ time_to_string_text (long time)
 
 	len_minutes = 0;
 
-	for (int n = 2; n < 60; n++)
+	for (n = 2; n < 60; n++)
 	{
 		if (n < 10)
 		{
