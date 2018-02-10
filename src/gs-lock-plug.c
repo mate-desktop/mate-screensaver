57b39ec7da232554b9fec7e5ee090693d8afbc77
Pablo Barciela <scow@riseup.net>
2018-02-10 14:40:25 +0100

    avoid deprecated GtkStock

--- src/gs-lock-plug.c
+++ src/gs-lock-plug.c
@@ -1587,7 +1587,11 @@ gs_lock_plug_add_button (GSLockPlug  *plug,
 	g_return_val_if_fail (GS_IS_LOCK_PLUG (plug), NULL);
 	g_return_val_if_fail (button_text != NULL, NULL);
 
-	button = gtk_button_new_with_label (button_text);
+	button = GTK_WIDGET (g_object_new (GTK_TYPE_BUTTON,
+	                                   "label", button_text,
+	                                   "use-stock", TRUE,
+	                                   "use-underline", TRUE,
+	                                   NULL));
 
 	gtk_widget_set_can_default (button, TRUE);
 
@@ -1664,7 +1668,7 @@ create_page_one_buttons (GSLockPlug *plug)
 
 	plug->priv->auth_cancel_button =  gs_lock_plug_add_button (GS_LOCK_PLUG (plug),
 	                                  plug->priv->auth_action_area,
-	                                  GTK_STOCK_CANCEL);
+	                                  "gtk-cancel");
 	gtk_widget_set_focus_on_click (GTK_WIDGET (plug->priv->auth_cancel_button), FALSE);
 
 	plug->priv->auth_unlock_button =  gs_lock_plug_add_button (GS_LOCK_PLUG (plug),
